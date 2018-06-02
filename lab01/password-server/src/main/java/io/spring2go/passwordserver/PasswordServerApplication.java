69aae3cb3478da70efb93baf9f363e08d30a6679
jw88 <jw88@live.com>
2018-06-01 21:01:49 -0700

    u

--- lab01/password-server/src/main/java/io/spring2go/passwordserver/PasswordServerApplication.java
+++ lab01/password-server/src/main/java/io/spring2go/passwordserver/PasswordServerApplication.java
@@ -10,3 +10,7 @@ public class PasswordServerApplication {
 		SpringApplication.run(PasswordServerApplication.class, args);
 	}
 }
+
+
+//curl -X POST --user clientapp:112233 "http://localhost:8080/oauth/token?grant_type=password&username=bobo&password=xyz&scope=read_userinfo" -H "accept: application/json" -H "content-type: application/x-www-formurlencoded"
+//curl -X GET http://localhost:8080/api/userinfo -H "authorization: Bearer 5f2be12e-8114-4370-a713-37b58e7c8753"
